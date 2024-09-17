1db432ef7b0ca014e5f5bc0e898356446c4c2f41
nikolay <n.smolov@yandex.ru>
2024-09-17 13:49:51 +0300

    complete compose deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = getenv('SECRET_KEY', 'django_sercret_key')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'taski.nks-tech.ru']
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
